f37c7d0cef8e5e419e7d614b1ed72afc6634d208
Ryan Ewen <ryan.ewen@gmail.com>
2021-09-18 17:28:53 +0000

    Add some color

--- src/client/components/GuestCard.js
+++ src/client/components/GuestCard.js
@@ -1,13 +1,10 @@
-import Button from '@material-ui/core/Button'
 import Card from '@material-ui/core/Card'
-import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
-import Grid from '@material-ui/core/Grid'
 import { makeStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
-import React, { useState } from 'react'
+import React from 'react'
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
     card: {
         display: 'flex',
         flexDirection: 'column',
@@ -20,7 +17,7 @@ const useStyles = makeStyles((theme) => ({
         display: 'flex',
         justifyContent: 'space-between',
     },
-}))
+})
 
 export default function GuestCard(props) {
     const { guest } = props
--- src/client/components/GuestListItem.js
+++ src/client/components/GuestListItem.js
@@ -3,11 +3,17 @@ import { makeStyles } from '@material-ui/core/styles'
 import Avatar from '@material-ui/core/Avatar'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemAvatar from '@material-ui/core/ListItemAvatar'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
 import Typography from '@material-ui/core/Typography'
+import stringToColor from 'string-to-color'
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
+    avatar: (props) => ({
+        backgroundColor: stringToColor(
+            // guest initials
+            props.guest.name.split(' ').map(name => name[0]).join('').toUpperCase()
+        ),
+    }),
     body: {
         display: 'block',
         whiteSpace: 'nowrap',
@@ -18,11 +24,11 @@ const useStyles = makeStyles((theme) => ({
         textAlign: 'right',
         // color: grey[500],
     },
-}))
+})
 
 export default function GuestListItem(props) {
     const { guest, onClick } = props
-    const classes = useStyles()
+    const classes = useStyles(props)
 
     function handleClick() {
         onClick(guest)
@@ -35,7 +41,7 @@ export default function GuestListItem(props) {
             onClick={handleClick}
         >
             <ListItemAvatar>
-                <Avatar>{guest.name[0].toUpperCase()}</Avatar>
+                <Avatar alt={guest.name} classes={{ root: classes.avatar }}>{guest.name[0].toUpperCase()}</Avatar>
             </ListItemAvatar>
             <ListItemText
                 primary={guest.name}
